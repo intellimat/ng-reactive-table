06cee1732e78513ab4f6723ca4706d2f5305b62c
Matteo <matnice23@gmail.com>
2023-02-12 18:35:28 +0100

    fixed style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 import { CardComponent } from './card/card.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CardComponent, ConfirmationDialogComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatDialogModule, MatButtonModule],
   exports: [CardComponent, ConfirmationDialogComponent],
 })
 export class SharedModule {}
