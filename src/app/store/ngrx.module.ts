c828f6d7fc3c686d360c74051918a578df032677
Matteo <matnice23@gmail.com>
2024-12-23 21:35:05 +1100

    upgraded_ngrx_to_v17

--- src/app/store/ngrx.module.ts
+++ src/app/store/ngrx.module.ts
@@ -22,6 +22,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
       autoPause: true,
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true,
     }),
     MatSnackBarModule,
   ],
